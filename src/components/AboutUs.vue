0f659e78c91d8dcd02516bd93f20935a47c77df3
王孟佳 <wangmengjia@wangmengjiadeMac-mini.local>
2018-06-27 10:22:28 +0800

    add

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -15,22 +15,22 @@
 import vueAccordion from './vue-accordion.vue'
 export default {
   name: 'AboutUs',
-  activated: function () {
-    this.$emit('switchroute', '/aboutus')
-  },
   components: {
     vueAccordion
+  },
+  activated: function () {
+    this.$emit('switchroute', '/aboutus')
   },
   data () {
     return {
       items: [
-        {image: '/static/1.png', width: 896.0},
-        {image: '/static/2.jpg', width: 449.0},
-        {image: '/static/3.jpg', width: 799.0},
-        {image: '/static/4.jpg', width: 799.0},
-        {image: '/static/5.jpg', width: 799.0},
-        {image: '/static/6.jpg', width: 799.0},
-        {image: '/static/7.jpg', width: 873.0}
+        {image: '/static/about/1.png', width: 896.0},
+        {image: '/static/about/2.jpg', width: 449.0},
+        {image: '/static/about/3.jpg', width: 799.0},
+        {image: '/static/about/4.jpg', width: 799.0},
+        {image: '/static/about/5.jpg', width: 799.0},
+        {image: '/static/about/6.jpg', width: 799.0},
+        {image: '/static/about/7.jpg', width: 873.0}
       ]
     }
   }
