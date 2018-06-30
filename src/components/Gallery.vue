f21cc6aa5dad759d384cb6fb9ade4c8090fb3645
zsly3n3 <zsly3n@163.com>
2018-07-01 04:52:46 +0800

    add

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -47,42 +47,7 @@
         <img class="little1" @click="showPhotoSwipe(11,imgs)" v-bind:src="imgs[11].url">
         <img class="little1" @click="showPhotoSwipe(12,imgs)" v-bind:src="imgs[12].url">
     </table>
-    <div class="pswp" tabindex="-1" role= "dialog" aria-hidden="true">
-            <div class="pswp__bg"></div>
-            <div class="pswp__scroll-wrap">
-                <div class="pswp__container">
-                    <div class="pswp__item"></div>
-                    <div class="pswp__item"></div>
-                    <div class="pswp__item"></div>
-                </div>
-                <div class="pswp__ui pswp__ui--hidden">
-                    <div class="pswp__top-bar">
-                        <div class="pswp__counter"></div>
-                        <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-                        <button class="pswp__button pswp__button--share" title="Share"></button>
-                        <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-                        <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-                        <div class="pswp__preloader">
-                            <div class="pswp__preloader__icn">
-                              <div class="pswp__preloader__cut">
-                                <div class="pswp__preloader__donut"></div>
-                              </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                        <div class="pswp__share-tooltip"></div>
-                    </div>
-                    <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
-                    </button>
-                    <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
-                    </button>
-                    <div class="pswp__caption">
-                        <div class="pswp__caption__center"></div>
-                    </div>
-                  </div>
-                </div>
-        </div>
+    <showPhoto></showPhoto>
   </div>
 </template>
 <script>
@@ -90,10 +55,14 @@ import 'photoswipe/dist/photoswipe.css'
 import 'photoswipe/dist/default-skin/default-skin.css'
 import PhotoSwipe from 'photoswipe/dist/photoswipe'
 import PhotoSwipeDefaultUI from 'photoswipe/dist/photoswipe-ui-default'
+import showPhoto from './others/showPhoto.vue'
 export default {
   name: 'Gallery',
   activated: function () {
     this.$emit('switchroute', '/gallery')
+  },
+  components: {
+    showPhoto
   },
   data () {
     return {
